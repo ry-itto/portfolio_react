19beb4586cde0c18f756cf406dfbdd34e1491502
ryoya_ito <horusu.dqx@gmail.com>
2019-03-03 03:06:44 +0900

    マージン追加

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,28 +9,30 @@ class Profile extends Component {
     return (
       <div>
         <Header />
-        <p className='display-4'>Profile</p>
-        {/* <img src={ facebook_profile_image } alt='My profile image' widrh='200px' height='200px' /> */}
-        <Table striped>
-          <tbody>
-            <tr>
-              <td>名前</td>
-              <td>伊藤 凌也</td>
-            </tr>
-            <tr>
-              <td>大学</td>
-              <td>会津大学</td>
-            </tr>
-            <tr>
-              <td>出身地</td>
-              <td>北海道</td>
-            </tr>
-            <tr>
-              <td>好きな言語</td>
-              <td>Ruby</td>
-            </tr>
-          </tbody>
-        </Table>
+        <div style={{margin: 30}}>
+          <p className='display-4'>Profile</p>
+          {/* <img src={ facebook_profile_image } alt='My profile image' widrh='200px' height='200px' /> */}
+          <Table striped>
+            <tbody>
+              <tr>
+                <td>名前</td>
+                <td>伊藤 凌也</td>
+              </tr>
+              <tr>
+                <td>大学</td>
+                <td>会津大学</td>
+              </tr>
+              <tr>
+                <td>出身地</td>
+                <td>北海道</td>
+              </tr>
+              <tr>
+                <td>好きな言語</td>
+                <td>Ruby</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
         <Footer />
       </div>
     )
