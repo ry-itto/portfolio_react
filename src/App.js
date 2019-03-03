023a6041c0a7a60d0032f16ebe63c7d4db8bce82
ryoya_ito <horusu.dqx@gmail.com>
2019-03-04 02:38:59 +0900

    Switch追加

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Welcome from './components/Welcome';
 import Profile from './components/Profile';
 import Link from './components/Link';
@@ -7,14 +7,16 @@ import Link from './components/Link';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Route exact path='/' component={Welcome} />
-          <Route path='/welcome' component={Welcome} />
-          <Route path='/profile' component={Profile} />
-          <Route path='/links' component={Link} />
-        </div>
-      </BrowserRouter>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Welcome} />
+            <Route path='/welcome' component={Welcome} />
+            <Route path='/profile' component={Profile} />
+            <Route path='/links' component={Link} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     )
   }
 }
