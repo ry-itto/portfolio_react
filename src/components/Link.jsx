0074feff42dd1f04fa04ae13ab4b1fbca715a4b6
ryoya_ito <horusu.dqx@gmail.com>
2019-03-04 17:56:26 +0900

    package.json修正，Linkページ修正

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardText, 
-  CardTitle, Button } from 'reactstrap';
+import { Card, CardHeader, CardBody, CardText, CardTitle, Button, Col, Row } from 'reactstrap';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -11,13 +10,23 @@ class Link extends Component {
         <Header />
         <div style={{margin: 30}}>
           <p className='display-4'>Links</p>
-          <div style={{width: 400}}>
-            <Card body>
-              <CardTitle>GitHub</CardTitle>
-              <CardText>GitHubのプロフィールページへのリンク</CardText>
-              <Button outline color='primary' href='https://github.com/ry-itto'>GitHub</Button>
-            </Card>
-          </div>
+          <Row>
+            <Col sm='6'>
+              <Card body>
+                <CardHeader>GitHub</CardHeader>
+                <CardBody>
+                  <CardText>GitHubのプロフィールページへのリンク</CardText>
+                  <Button outline color='primary' href='https://github.com/ry-itto'>GitHub</Button>
+                </CardBody>
+              </Card>
+            </Col>            
+            <Col sm='6'>
+              <Card body>
+                <CardTitle>ポートフォリオのリポジトリ</CardTitle>
+                <Button outline color='primary' href='https://github.com/ry-itto/portfolio_react'>GitHub</Button>
+              </Card>
+            </Col>
+          </Row>
         </div>
         <Footer />
       </div>
